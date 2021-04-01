cda0db06cb0b3c1b2b351330be614f5811eb8cb6
Armend Lesi <armend.lesi@gmail.com>
2021-04-01 09:11:12 +0200

    add PoC router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Calendar from './components/Calendar';
-import ReduxExample from './pages/ReduxExample';
-import OpenAPI from './pages/OpenAPI';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+  withRouter,
+} from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+import Projects from './pages/Projects';
+import Administration from './pages/Administration';
+import TimeTracking from './pages/TimeTracking';
 
 function App() {
-  return (
-    <div className="App">
-      <Calendar />
-      <ReduxExample />
-      <OpenAPI />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const isAuthenticated = true;
 
-    </div>
+  const MenuBar = withRouter(() => {
+    if (isAuthenticated) {
+      return (
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/dashboard">Mein Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/timetracking">Meine Zeiterfassung</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projekte</Link>
+              </li>
+              <li>
+                <Link to="/administration">Administration</Link>
+              </li>
+              <li>
+                <Link to="/singout">Ausloggen</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      );
+    }
+    return null;
+  });
+
+  return (
+    <Router>
+      <MenuBar />
+      <Switch>
+        <Redirect exact from="/" to="/dashboard" />
+        <Route path="/login" component={Login} />
+
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/timetracking" component={TimeTracking} />
+        <PrivateRoute path="/projects" component={Projects} />
+        <PrivateRoute path="/administration" component={Administration} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Administration.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Administration() {
+  return (
+    <div>
+      <h1> Administration </h1>
+    </div>
+  );
+}
+
+export default Administration;
