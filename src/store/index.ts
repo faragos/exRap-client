6468e2e9af1106b9a0654ed441acab3ed3ecc53c
Armend Lesi <armend.lesi@gmail.com>
2021-04-02 10:34:25 +0200

    add authApi

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,16 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
 import { api as dashboardApi } from '../service/dashboard.api';
+import { api as authApi } from '../gen/auth.api.generated';
+
 // @ts-ignore
 export type RootState = ReturnType;
 
 const index = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(dashboardApi.middleware),
+
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(dashboardApi.middleware, authApi.middleware),
 });
 
 export type AppDispatch = typeof index.dispatch;
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/reducers';
 import { api as dashboardApi } from '../service/dashboard.api';
+import { api as authApi } from '../service/auth.api';
 
 const rootReducer = combineReducers({
   user: userReducer,
   [dashboardApi.reducerPath]: dashboardApi.reducer,
+  [authApi.reducerPath]: authApi.reducer,
 });
 
 // @ts-ignore
