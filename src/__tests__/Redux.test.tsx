6d9b0288996484500011bbcf952f49302ee0e0f3
Armend Lesi <armend.lesi@gmail.com>
2021-04-04 16:13:27 +0200

    add redux-test

---
+++ src/__tests__/Redux.test.tsx
@@ -0,0 +1,37 @@
+import { cleanup } from '@testing-library/react';
+import index from '../store/index';
+import {
+  setCredentials,
+  logoutUser,
+} from '../store/user/reducers';
+
+afterEach(cleanup);
+test('set user credantials to redux store', () => {
+  const testUser = {
+    username: 'TestUser',
+    password: '1234Test',
+    token: 'asdfewrabaer5t24q5g',
+    isAuthenticated: true,
+  };
+  const expectedUser = {
+    username: 'TestUser',
+    token: 'asdfewrabaer5t24q5g',
+    password: '',
+    isAuthenticated: true,
+  };
+  index.dispatch(setCredentials(testUser));
+  const { user } = index.getState();
+  expect(user).toMatchObject(expectedUser);
+});
+test('logoutUser', () => {
+  const testUser = {
+    username: 'TestUser',
+    password: '1234Test',
+    token: 'asdfewrabaer5t24q5g',
+    isAuthenticated: true,
+  };
+  index.dispatch(setCredentials(testUser));
+  index.dispatch(logoutUser());
+  const { user } = index.getState();
+  expect(user.isAuthenticated).toBeFalsy();
+});
