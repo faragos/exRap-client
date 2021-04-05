e8959a38526dd08739912d282cc548a331533b77
Lukas Schlunegger <lukas.schlunegger@gmail.com>
2021-04-05 17:58:48 +0200

    Add Sidebar tests

---
+++ src/__tests__/Sidebar.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { cleanup } from '@testing-library/react';
+import Sidebar from '../components/Sidebar';
+
+afterEach(cleanup);
+test('move from dashboard to projects', (async) => {
+  const div = document.createElement('div');
+});
