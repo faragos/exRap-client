3de9005565d86022b550ea527a75d4a63a817d94
Lukas Schlunegger <lukas.schlunegger@gmail.com>
2021-04-05 20:06:49 +0200

    delete sidebar test

--- src/__tests__/Sidebar.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { cleanup } from '@testing-library/react';
-import Sidebar from '../components/Sidebar';
-
-afterEach(cleanup);
-test('move from dashboard to projects', (async) => {
-  const div = document.createElement('div');
-});
