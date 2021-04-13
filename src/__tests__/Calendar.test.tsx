58ecb867f7dc3a2337134043d6f23775cd48489f
Armend Lesi <armend.lesi@gmail.com>
2021-04-13 19:34:54 +0200

    update calendar test

--- src/__tests__/Calendar.test.tsx
+++ src/__tests__/Calendar.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { cleanup } from '@testing-library/react';
+import { render, screen } from './test-utils';
 import Calendar from '../components/Calendar';
 
 afterEach(cleanup);
+
 test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Calendar />, div);
+  render(<Calendar />);
+  expect(screen.getByText(/Heute/i)).toBeInTheDocument();
 });
