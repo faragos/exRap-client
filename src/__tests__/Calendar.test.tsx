2cd20d2939660b02cf99747d1c3c185e94f92ed7
Armend Lesi <armend.lesi@gmail.com>
2021-04-15 08:08:03 +0200

    exchange test-utils.tsx trough @testing-library/react functions

--- src/__tests__/Calendar.test.tsx
+++ src/__tests__/Calendar.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { cleanup } from '@testing-library/react';
-import { render, screen } from '../test-utils/test-utils';
+import { cleanup, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import Calendar from '../components/Calendar';
+import store from '../store/store';
 
 afterEach(cleanup);
 
 test('renders without crashing', () => {
-  render(<Calendar />);
-  expect(screen.getByText(/Heute/i)).toBeInTheDocument();
+  render(<Provider store={store}><Calendar /></Provider>);
+  const linkElement = screen.getByText(/Heute/i);
+  expect(linkElement).toBeInTheDocument();
 });
