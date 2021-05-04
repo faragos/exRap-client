f79db25efa5a50095601d95e98ff20934a670051
Armend Lesi <armend.lesi@gmail.com>
2021-05-04 20:08:10 +0200

    remove unnecessary code

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import FullCalendar, { DatesSetArg, EventClickArg } from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
